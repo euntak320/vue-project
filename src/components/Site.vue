27853fca8fc32938b0939bd1f476cc28dd22ab23
한은탁 <ethan@han-euntag-ui-MacBookPro.local>
2024-12-21 18:40:50 +0900

    컴포넌트 셋팅 및 기타 작업

---
+++ src/components/Site.vue
@@ -0,0 +1,4 @@
+<template>
+
+    site
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+import Header from '@/components/Header.vue';
+import Contact from '@/components/Contact.vue';
+import Footer from '@/components/Footer.vue';
+import Skill from '@/components/skill.vue';
+import Skip from '@/components/Skip.vue';
+import Intro from '@/components/intro.vue';
+import Port from '@/components/Port.vue';
+import Site from '@/components/Site.vue';
+</script>
+
 <template>
-  <main>
-    Home
+  <Skip />
+  <Header />
+  <main id ="main" role="main">
+    <Intro />
+    <Skill />
+    <Port />
+    <Site />
+    <Contact />
   </main>
+  <Footer />
 </template>
